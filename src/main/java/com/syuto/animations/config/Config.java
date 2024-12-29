a93a227cd9195dde90cbbc433e0be97fbe7f8f4b
Syuto <110631856+SyutoBestCoder@users.noreply.github.com>
2024-12-29 18:24:36 +0000

    d
    d

--- src/main/java/com/syuto/animations/config/Config.java
+++ src/main/java/com/syuto/animations/config/Config.java
@@ -5,5 +5,5 @@ import com.syuto.animations.config.api.AnimationMode;
 public class Config {
     public static AnimationMode mode = AnimationMode.VANILLA;
     public static int scale = 100;
-    public static int swingSpeed = 100;
+    public static int swingSpeed = 1;
 }
